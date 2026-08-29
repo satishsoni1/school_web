a7f867b24a4a6706ae39346c2fe76ade3dbcd2cf
Satish Soni <sonisatish119@gmail.com>
2026-08-29 22:01:04 +0530

    config

--- mvc/config/onesignal.php
+++ mvc/config/onesignal.php
@@ -4,5 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // OneSignal credentials used by Notification_lib (mvc/libraries/Notification_lib.php)
 // to push notifications and by app.component.ts (OneSignal.initialize) on the client.
 // Moved out of Notice.php, where these were previously hardcoded inline.
-$config['onesignal_app_id'] = getenv('ONESIGNAL_APP_ID');
-$config['onesignal_api_key'] = getenv('ONESIGNAL_API_KEY');
+
+// Default values
+$config['onesignal_app_id'] = '';
+$config['onesignal_api_key'] = '';
+
+// Load local configuration if available
+$local_config = APPPATH . 'config/onesignal_local.php';
+
+if (file_exists($local_config)) {
+    require $local_config;
+}
